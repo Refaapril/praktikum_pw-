d5d612920dafd809b340e2f7a8470cd96559f593
Refaapril <refaapriliani87@gmail.com>
2025-12-02 17:34:12 +0700

    Add database connection file conn.php

---
+++ praktikum9/conn.php
@@ -0,0 +1,13 @@
+<?php
+// File: conn.php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db   = "pagination";
+
+$conn = mysqli_connect($host, $user, $pass, $db);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+?>
